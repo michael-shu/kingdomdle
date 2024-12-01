162321d2bf885a0fe71ac2bd673cccfd02e75eb2
Michael Shu <86576359+michael-shu@users.noreply.github.com>
2024-12-01 17:50:58 -0500

    add logging

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,5 +1,6 @@
 export const dynamic = 'force-dynamic'; // static by default, unless reading the request
  
 export function GET() {
+    console.log("What is going on here...");
   return new Response(`Hello from ${process.env.VERCEL_REGION}`);
 }
